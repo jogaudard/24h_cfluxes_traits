f46b8de92e83070554b61f9ba60b519d7b0f9a05
jogaudard <joseph.gaudard@pm.me>
2023-04-19 10:54:01 +0200

    first commit. description of the aims

---
+++ prepare_data.R
@@ -0,0 +1,3 @@
+# This script is to prepare several dataset to test the SEM.
+# The main issue will be to decide how to group the data based on the time of the day.
+# Because of a lack of replicate, we have to group the data and cannot use the hourly dataset.
