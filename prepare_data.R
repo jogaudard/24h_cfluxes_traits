d7fffa71f080c2769c55102b8635ba59bc1ba5bf
jogaudard <joseph.gaudard@pm.me>
2023-04-19 17:11:28 +0200

    wrong branch

--- prepare_data.R
+++
@@ -1,3 +0,0 @@
-# This script is to prepare several dataset to test the SEM.
-# The main issue will be to decide how to group the data based on the time of the day.
-# Because of a lack of replicate, we have to group the data and cannot use the hourly dataset.
